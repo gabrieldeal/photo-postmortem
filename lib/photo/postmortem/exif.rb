763b158014714c062be8cd8392e71ef23ebdb389
Gabriel M Deal <gabriel@dicksonlabs.com>
2017-06-14 13:45:39 -0700

    Optimize and add ability to count all photos

---
+++ lib/photo/postmortem/exif.rb
@@ -0,0 +1,32 @@
+require "exif"
+require "mini_exiftool_vendored"
+
+module Photo
+  module Postmortem
+    class Exif
+      attr_accessor :creation_time,
+                    :creator
+
+      def initialize(photo_path)
+        initialize_fast(photo_path) || initialize_comprehensive(photo_path)
+      end
+
+      # This is fast but doesn't support many image formats (like DNG).
+      def initialize_fast(photo_path)
+        exif = ::Exif::Data.new(photo_path)
+        self.creation_time = exif.date_time_original
+        self.creator = exif.artist
+        true
+      rescue
+        return false
+      end
+
+      # This is slow but supports almost all image formats.
+      def initialize_comprehensive(photo_path)
+        exif = MiniExiftool.new(photo_path)
+        self.creation_time = exif.create_date
+        self.creator = exif.artist
+      end
+    end
+  end
+end
--- photo-postmortem.gemspec
+++ photo-postmortem.gemspec
@@ -36,6 +36,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rake", "~> 10.0"
 
   spec.add_runtime_dependency "activesupport", "~> 5.1"
+  spec.add_runtime_dependency "exif", "~> 1.0"
   spec.add_runtime_dependency "mini_exiftool_vendored", "~> 9.2"
   spec.add_runtime_dependency "spreadsheet", "~> 1.1"
 end
