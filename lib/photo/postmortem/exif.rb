20be320e8600281510e46fe9f00b90bdfdac0be2
Gabriel M Deal <gabriel@dicksonlabs.com>
2019-03-03 17:51:55 -0800

    Add focal length to spreadsheet.
    And add ability to specify multiple --all-photos-directory options.

--- lib/photo/postmortem/exif.rb
+++ lib/photo/postmortem/exif.rb
@@ -7,8 +7,10 @@ module Photo
   module Postmortem
     # Hide the difference between the different EXIF parsers we use.
     class Exif
-      attr_accessor :creation_time,
-                    :creator
+      attr_accessor :camera,
+                    :creation_time,
+                    :creator,
+                    :focal_length
 
       def initialize(photo_path)
         initialize_fast(photo_path) || initialize_comprehensive(photo_path)
@@ -19,6 +21,7 @@ module Photo
         exif = ::Exif::Data.new(photo_path)
         self.creation_time = exif.date_time_original
         self.creator = exif.artist
+        print 'FIXME: What are the focal length & camera fields?'
         true
       rescue ::Exif::NotReadable
         false
@@ -29,6 +32,10 @@ module Photo
         exif = MiniExiftool.new(photo_path)
         self.creation_time = exif.create_date
         self.creator = exif.artist
+        self.focal_length = exif.focallengthin35mmformat
+        self.camera = exif.uniquecameramodel ||
+                      "#{exif.make} #{exif.model}" ||
+                      'Unknown'
       end
     end
   end
